b87e0e85dd6d40c6d995aadf80f5f7e5bcaec08a
mahedikd <mahedi66958@gmail.com>
2025-07-16 06:10:56 +0000

    Updates

--- assets/Resume-B2YeElXe.js
+++ assets/Resume-B2YeElXe.js
@@ -1,4 +1,4 @@
-import{b as t,j as e,D as a}from"./index-DrXzGtgL.js";import{S as m}from"./Seo-DSD2srJE.js";/**
+import{b as t,j as e,D as a}from"./index-BxWHzEu0.js";import{S as m}from"./Seo-D7us4JLO.js";/**
  * @license lucide-react v0.453.0 - ISC
  *
  * This source code is licensed under the ISC license.
